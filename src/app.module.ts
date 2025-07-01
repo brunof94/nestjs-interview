fe076fc40054cb8a5d1886a941312d44819ae980
Brian Stefanovich <stefanovich.brian@gmail.com>
2025-07-01 22:22:28 +0000

    feat/setup-type-orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TodoListsModule } from './todo_lists/todo_lists.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TodoListsModule],
+  imports: [
+    TodoListsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
